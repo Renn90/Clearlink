a7f6066849502b108bf53c422073e045ce9ace6c
faithful <faithfulokondu@gmail.com>
2024-01-21 21:34:58 +0100

    tailwid config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        lt: '350px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        "max-w-xl": "1080px",
+      },
+      colors: {
+       primary: '#175CD3',
+       white: '#FFFFFF'
+    },
+    },
   },
   plugins: [],
 }
