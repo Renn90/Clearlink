60f5e950d74c4d5dde1d46e5106a2671c4bc1881
faithful <faithfulokondu@gmail.com>
2024-01-23 22:13:30 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-icons'],
-    },
-  },
-  plugins: [react()]
+  plugins: [react()],
+  build:{
+    outDir: "build"
+  }
 })
